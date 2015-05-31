d432cdcb0682fc5fbd4d38bedbf499596536339c
Chris Schmich <schmch@gmail.com>
2015-05-30 23:48:47 -0500

    Add lookup by user ID.

--- id.js
+++ id.js
@@ -4,7 +4,9 @@ var async = nazgul.async,
     Log = nazgul.Log,
     sprintf = nazgul.sprintf,
     URI = nazgul.URI,
-    Twitch = nazgul.Twitch;
+    Twitch = nazgul.Twitch,
+    MongoClient = nazgul.MongoClient,
+    Config = nazgul.Config;
 
 var showUserId = async(function(username) {
   var url = URI(sprintf('https://api.twitch.tv/kraken/users/%s', username)).toString();
@@ -12,9 +14,29 @@ var showUserId = async(function(username) {
   Log.info(sprintf('ID: %d', response._id));
 });
 
+var showUserName = async(function(id) {
+  var db = await(MongoClient.connectAsync(Config.mongo));
+  var snapshots = db.collection('stream:snapshots');
+
+  Log.info(sprintf('Finding user #%d', id));
+  var doc = await(snapshots.findOneAsync({ ui: id }, { ch: 1 }));
+  if (doc && doc.ch) {
+    Log.info(sprintf('User: %s', doc.ch));
+  } else {
+    Log.error('User not found.');
+  }
+
+  db.close();
+});
+
 if (process.argv.length < 3) {
-  Log.error('Username expected.');
+  Log.error('Username or ID expected.');
   process.exit(1);
 } else {
-  showUserId(process.argv[2]);
+  var arg = process.argv[2];
+  if (+arg == arg) {
+    showUserName(+arg).done();  
+  } else {
+    showUserId(arg).done();
+  } 
 }
