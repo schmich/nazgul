1f251ccfb9b3ef1902f22e334ef3fa10cb22c23f
Chris Schmich <schmch@gmail.com>
2015-06-10 17:02:02 -0500

    Fix host snapshot to use new Twitch API endpoint.

--- snapshot-hosts.js
+++ snapshot-hosts.js
@@ -8,17 +8,37 @@ var nazgul = require('./nazgul'),
     MongoClient = nazgul.MongoClient,
     Twitch = nazgul.Twitch;
 
+var twitchId = async(function(user, snapshots) {
+  var stream = await(snapshots.findOneAsync({ ch: user }, { ui: 1 }));
+  if (stream && stream.ui) {
+    id = stream.ui;
+  } else {
+    var url = sprintf('https://api.twitch.tv/kraken/users/%s', user);
+    var response = await(Twitch.request(url));
+    id = response._id;
+  }
+
+  if (!id) {
+    throw new Error(sprintf('Could not find Twitch channel ID for %s.', channel));
+  }
+
+  return id;
+});
+
 var takeSnapshot = async(function() {
   Log.info('Host snapshot starting.');
   var db = await(MongoClient.connectAsync(Config.mongo));
   var hosts = db.collection('host:snapshots');
+  var snapshots = db.collection('stream:snapshots');
 
   var timestamp = Date.now();
 
   var channels = Config.channels;
   for (var i = 0; i < channels.length; ++i) {
     var channel = channels[i];
-    var url = sprintf('http://chatdepot.twitch.tv/rooms/%s/hosts', channel);
+    var targetId = await(twitchId(channel, snapshots));
+
+    var url = sprintf('http://tmi.twitch.tv/hosts?include_logins=1&target=%s', targetId);
 
     var response = await(Twitch.request(url, false));
 
@@ -26,7 +46,7 @@ var takeSnapshot = async(function() {
       var doc = {
         c: channel,
         t: timestamp,
-        h: _(response.hosts).map('host').value()
+        h: _(response.hosts).map('host_login').value()
       };
 
       await(hosts.insertAsync(doc));
